c267286124d6c920f604173623fbc9c05b0452ab
Michelle Dicks <michd@alumni.stanford.edu>
2017-09-04 21:01:22 -0700

    first attempt at HW problem one

---
+++ CandyBarSales.cpp
@@ -0,0 +1,24 @@
+// Candy Bar Sales 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double candyBarsSold, profitPerCandyBar, earnings;
+
+	// Get the number of candy bars sold.
+	cout << "How many candy bars were sold?";
+	cin >> candyBarsSold;
+	
+	// Get the amount of profit.
+	cout << "How much profit is generated from each candy bar sold?";
+	cin >> profitPerCandyBar;
+
+	// Calculate the profit.
+	earnings = candyBarsSold * profitPerCandyBar;
+
+	// Display the earnings.
+	cout << "You have earned $" << earnings << endl; 
+
+	return 0;
+}
