c210eb67deec15d73912743fa532e72e9955629d
Michelle Dicks <michd@alumni.stanford.edu>
2017-09-20 20:10:50 -0700

    finished Personal Best Program

--- PersonalBest.cc
+++ PersonalBest.cc
@@ -6,128 +6,126 @@ using namespace std;
 int main ()
 {
 	//Initalize variables
-	string name, date1, date2, date3; 
-	double height1, height2, height3; 
+	string name, date1, date2, date3, firstDate, secondDate, thirdDate; 
+	double height1, height2, height3, first, second, third; 
 
 	// Explain purpose of program
-	cout << "This program takes the names of one pole vaulter,\n"
+	cout << "This program takes the name of one pole vaulter,\n"
 			"and the dates and heights of three jumps.\n"
-			"It will then list the first, second and third place vaults.\n";
+			"It will then list the first, second, and third place jumps.\n";
 	
 	// Obtain the name of the pole vaulter
 	cout << "Please enter the name of the pole vaulter: ";
 	getline(cin, name);
+    cout << endl;
 	
-	// Obtain the date & height of the first pole vault
-	cout << "\nPlease enter the date of the first pole vault."
-	cin.ignore();
-    getline(cin, date1);
-	cout << "\nPlease enter the height of the first pole vaultin meters: ";
+	// Obtain the date & height of the first pole vault & validate height
+	cout << "Please enter the date of the first pole vault: ";
+	getline(cin, date1);
+	cout << "\nPlease enter the height of the first pole vault in meters: ";
 	cin >> height1;
 	if ((height1 < 2) || (height1 > 5)) 	
 	{	cout << "\nThe height cannot be less than 2 meters or greater than 5 meters"
-			 "\nPlease enter a conforming finish time: ";
+			 "\nPlease enter a conforming height: ";
 		cin >> height1;
 	}
 
-	//left off here
-	// Obtain user input for runner #2 & validate time
-	cout << "Please enter the name of the second runner: ";
+	// Obtain the date & height of the second pole vault & validate height
+	cout << "\nPlease enter the date of the second pole vault: ";
 	cin.ignore();
-    getline(cin, runner2);
-	cout << "Please enter the second runner's finish time: ";
-	cin >> speed2;
-	if (speed2 <= 0)
-	{	cout << "\nThe finish time must be positive. "
-			 "\nPlease enter a positive finish time: ";
-		cin >> speed2;
+    getline(cin, date2);
+	cout << "\nPlease enter the height of the second pole vault in meters: ";
+	cin >> height2;
+	if ((height2 < 2) || (height2 > 5)) 	
+	{	cout << "\nThe height cannot be less than 2 meters or greater than 5 meters"
+			 "\nPlease enter a conforming height: ";
+		cin >> height2;
 	}
 
-	// Obtain user input for runner #3 & validate time
-	cout << "Please enter the name of the third runner: ";
+	// Obtain the date & height of the third pole vault & validate height
+	cout << "\nPlease enter the date of the third pole vault: ";
 	cin.ignore();
-    getline(cin, runner3);
-	cout << "Please enter the third runner's finish time: ";
-	cin >> speed3;
-	if (speed3 <= 0)
-	{	cout << "\nThe finish time must be positive. "
-			 "\nPlease enter a positive finish time: ";
-		cin >> speed3;
+    getline(cin, date3);
+	cout << "\nPlease enter the height of the third pole vault in meters: ";
+	cin >> height3;
+	if ((height3 < 2) || (height3 > 5)) 	
+	{	cout << "\nThe height cannot be less than 2 meters or greater than 5 meters"
+			 "\nPlease enter a conforming height: ";
+		cin >> height3;
 	}
-	//Test #1
-	// cout << "\nRunners names: " << runner1 << " " << runner2 << " " << runner3;
- 	//cout << "\nRunners times: " << speed1 << " " << speed2 << " " << speed3; 
 
     // Determine rankings
-    if ((speed1 < speed2) && (speed1 < speed3))
-    {	if (speed2 < speed3)
-		{	first = runner1;
-			firstt = speed1;
-			second = runner2;
-			secondt = speed2;
-			third = runner3;
-			thirdt = speed3;
+    if ((height1 > height2) && (height1 > height3))
+    {	if (height2 > height3)
+		{	first = height1;
+			firstDate = date1;
+			second = height2;
+			secondDate = date2;
+			third = height3;
+			thirdDate = date3;
 		}
 		else
-	  	{	first = runner1;
-	    	firstt = speed1;
-	    	second = runner3;
-	    	secondt = speed3;
-	    	third = runner2;
-	    	thirdt = speed2;
+	  	{	first = height1;
+	    	firstDate = date1;
+	    	second = height3;
+	    	secondDate = date3;
+	    	third = height2;
+	    	thirdDate = date2;
     	}
     }
-    else if ((speed2 < speed1) && (speed2 < speed3))
-    {	if (speed1 < speed3)
-    	{	first = runner2;
-	    	firstt = speed2;
-	    	second = runner1;
-	    	secondt = speed1;
-	    	third = runner3;
-	    	thirdt = speed3;
+    else if ((height2 > height1) && (height2 > height3))
+    {	if (height1 > height3)
+    	{	first = height2;
+	    	firstDate = date2;
+	    	second = height1;
+	    	secondDate = date1;
+	    	third = height3;
+	    	thirdDate = date3;
     	}
     	else
-    	{	first = runner2;
-    		firstt = speed2;
-    		second = runner3;
-    		secondt = speed3;
-    		third = runner1;
-    		thirdt = speed1;
+    	{	first = height2;
+    		firstDate = date2;
+    		second = height3;
+    		secondDate = date3;
+    		third = height1;
+    		thirdDate = date1;
     	}
     }
-	else if ((speed3 < speed1) && (speed3 < speed2))
-    {	if (speed1 < speed2)
-    	{	first = runner3;
-    		firstt = speed3;
-    		second = runner1;
-    		secondt = speed1;
-    		third = runner2;
-    		thirdt = speed2;
+	else if ((height3 > height1) && (height3 > height2))
+    {	if (height1 > height2)
+    	{	first = height3;
+    		firstDate = date3;
+    		second = height1;
+    		secondDate = date1;
+    		third = height2;
+    		thirdDate = date2;
     	}
     	else
-    	{	first = runner3;
-    		firstt = speed3;
-    		second = runner2;
-    		secondt = speed2;
-    		third = runner1;
-    		thirdt = speed1;
+    	{	first = height3;
+    		firstDate = date3;
+    		second = height2;
+    		secondDate = date2;
+    		third = height1;
+    		thirdDate = date1;
     	}
     }
     // Create table to display results
 	
 	cout << endl;
-	cout << left << setw(37) << "First Place:"
-		 << right << first << endl;
-	cout << left << setw(37) << "First Place Time:"
-		 << right << fixed << showpoint << firstt << endl;
-	cout << left << setw(37) << "Second Place:"
-		 << right << second << endl;
-	cout << left << setw(37) << "Second Place Time:"
-		 << right << fixed << showpoint << secondt << endl;
-	cout << left << setw(37) << "Third Place:"	 
-		 << right << third << endl;
-	cout << left << setw(37) << "Third Place Time:"
-		 << right << fixed << showpoint << thirdt << endl;
-	
+	cout << left << setw(45) << "Name of Pole Vaulter: "
+		 << right << name << endl;
+	cout << left << setw(45) << "Height of First Place (Best) Pole Vault:"
+		 << right << fixed << showpoint << first << " meters" << endl;
+	cout << left << setw(45) << "Date of Best Pole Vault:"
+		 << right << firstDate << endl;
+	cout << left << setw(45) << "Height of Second Place Pole Vault:"
+		 << right << fixed << showpoint << second << " meters" << endl;
+	cout << left << setw(45) << "Date of Second Place Pole Vault:"
+		 << right << secondDate << endl;
+	cout << left << setw(45) << "Height of Third Place (Worst) Pole Vault:"
+		 << right << fixed << showpoint << third << " meters" << endl;
+	cout << left << setw(45) << "Date of Third Place (Worse) Pole Vault:"
+		 << right << thirdDate << endl;
+
 	return 0;
 }
