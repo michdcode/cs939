b99a1bac59fb32a9a01a245b9cf5f6083b74807a
Michelle Dicks <michd@alumni.stanford.edu>
2017-09-27 15:46:31 -0700

    finished program

--- savingsAccount.cpp
+++ savingsAccount.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
 	// Initialize global variables
-	double startingBal, annualIntRate, currentBal, monthlyIntRate, totalDeposits, totalWithdrawls, 
-		   totalIntEarned, monthlyStartingBal;
+	double startingBal, annualIntRate, currentBal, monthlyIntRate, totalDeposits = 0, totalWithdrawls = 0, 
+		   totalIntEarned = 0, monthlyStartingBal;
 	
 	// Explain program 
 	cout << "This program tracks your savings account for three months.\n"
@@ -19,7 +19,7 @@ int main()
 	cout << "Please enter the starting balance: $";
 	cin >> startingBal;
 	
-	while (startingBal < 0)
+	while (startingBal <= 0)
 	{
 		cout << "\nThe starting balance should not be negative!";
 		cout << "\nPlease enter a starting balance greater than zero: $";
@@ -30,7 +30,7 @@ int main()
 	cout << "\nPlease enter the Annual Interest Rate, do not enter the percentage sign: ";
 	cin >> annualIntRate;
 	
-	while (annualIntRate < 0)
+	while (annualIntRate <= 0)
 	{
 		cout << "\nThe annual interest rate must be positive!\n"
 		        "Please enter an annual interest rate greater than zero,\n"
@@ -39,9 +39,10 @@ int main()
 	}
 
 	// Calculate the Monthly Interest Rate and set the current Balance
-	monthlyIntRate = (annualIntRate / 12);
-	startingBal = currentBal;
-	startingBal = monthlyStartingBal;
+	monthlyIntRate = (annualIntRate / 12.00)/100;
+	currentBal = startingBal;
+	monthlyStartingBal = startingBal;
+	cout << monthlyIntRate << endl;
 
 	// Obtain monthly values
 	for (int month = 1; month < 4; month++)
@@ -64,7 +65,7 @@ int main()
 		cout << "\nHow much money was withdrawn in month " << month << "? $";
 		cin >> monthlyWithdrawl;
 
-		while (monthlyWithdrawl < 0 || > currentBal)
+		while (monthlyWithdrawl < 0 || monthlyWithdrawl > currentBal)
 		{
 			cout <<"\nThe monthly withdrawl cannot be less than zero or\n"
 			       "greater than the balance available.\n"
@@ -77,24 +78,28 @@ int main()
 
 		// Calculate average balance, monthly interest, add to total interest & balance, reset monthlystart
 		double averageBal, monthlyInterest;
-		averageBal = (monthlyStartingBal + currentBal) / 2;
-		monthlyInterest = monthlyIntRate * averageBal;
+		averageBal = (monthlyStartingBal + currentBal)/2;
+		cout << averageBal << "Average Balance" << endl;
+		monthlyInterest = (monthlyIntRate * averageBal);
+		cout << monthlyInterest << "Monthly Interest " << endl;
 		totalIntEarned = totalIntEarned + monthlyInterest;
+		cout << totalIntEarned << "Total Interest Earned " << endl;
 		currentBal = currentBal + monthlyInterest;
-		monthlystart = currentBal;
+		monthlyStartingBal = currentBal;
+
 	}
 	// Create table to display results
 	cout << endl;
-	cout << left << set(35) << "Starting Balance: $"
-		 << right << startingBal << endl;
-	cout << left << set(35) << "Total Deposits: $"
-		 << right << totalDeposits << endl;
-	cout << left << set(35) << "Total Withdrawls: $"
-		 << right << totalWithdrawls << endl;
-	cout << left << set(35) << "Total Interest Earned: $"
-		 << right << totalIntEarned << endl;
-	cout << left << set(35) << "Final Balance: $"
-		 << right << currentBal << endl;
+	cout << left << setw(25) << "Starting Balance: "
+		 << right << "$" << startingBal << endl;
+	cout << left << setw(25) << "Total Deposits: "
+		 << right << "$" << totalDeposits << endl;
+	cout << left << setw(25) << "Total Withdrawls: "
+		 << right << "$" <<totalWithdrawls << endl;
+	cout << left << setw(25) << "Total Interest Earned: "
+		 << right << "$" <<totalIntEarned << endl;
+	cout << left << setw(25) << "Final Balance: "
+		 << right << "$" <<currentBal << endl;
 
 	return 0;
 }
