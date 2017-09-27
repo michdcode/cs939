72ad61526c25c254083f45a55ca7c1dd036c4633
Michelle Dicks <michd@alumni.stanford.edu>
2017-09-27 10:57:41 -0700

    finish program, not yet tested.

---
+++ savingsAccount.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	// Initialize global variables
+	double startingBal, annualIntRate, currentBal, monthlyIntRate, totalDeposits, totalWithdrawls, 
+		   totalIntEarned, monthlyStartingBal;
+	
+	// Explain program 
+	cout << "This program tracks your savings account for three months.\n"
+		 	"You will enter the starting balance and annual intrest rate.\n"
+			"Then, you will enter deposits and withdrawls for the three months.\n"
+			"The program will then display results for the three months.\n";
+	
+	// Gather & validate starting balance and annual interest rate
+	cout << "Please enter the starting balance: $";
+	cin >> startingBal;
+	
+	while (startingBal < 0)
+	{
+		cout << "\nThe starting balance should not be negative!";
+		cout << "\nPlease enter a starting balance greater than zero: $";
+		// cin.ignore();
+		cin >> startingBal;
+	}
+
+	cout << "\nPlease enter the Annual Interest Rate, do not enter the percentage sign: ";
+	cin >> annualIntRate;
+	
+	while (annualIntRate < 0)
+	{
+		cout << "\nThe annual interest rate must be positive!\n"
+		        "Please enter an annual interest rate greater than zero,\n"
+				"without the percentage sign: ";
+		cin >> annualIntRate;
+	}
+
+	// Calculate the Monthly Interest Rate and set the current Balance
+	monthlyIntRate = (annualIntRate / 12);
+	startingBal = currentBal;
+	startingBal = monthlyStartingBal;
+
+	// Obtain monthly values
+	for (int month = 1; month < 4; month++)
+	{	// Gather & validate deposits, add to deposit totals & add to current balance
+		double monthlyDeposit;
+		cout << "\nHow much money was deposited in month " << month << "? $";
+		cin >> monthlyDeposit;
+
+		while (monthlyDeposit < 0)
+		{
+			cout << "\nThe monthly deposit must be greater than zero!\n"
+				    "Please enter a monthly deposit greater than zero: $";
+			cin >> monthlyDeposit;
+		}
+		totalDeposits = totalDeposits + monthlyDeposit;
+		currentBal = currentBal + monthlyDeposit;
+
+		// Gather & validate withdrawls, add to withdrawl totals & subtract from current balance
+		double monthlyWithdrawl;
+		cout << "\nHow much money was withdrawn in month " << month << "? $";
+		cin >> monthlyWithdrawl;
+
+		while (monthlyWithdrawl < 0 || > currentBal)
+		{
+			cout <<"\nThe monthly withdrawl cannot be less than zero or\n"
+			       "greater than the balance available.\n"
+			       "Please enter a monthly withdrawl greater than zero and\n"
+			       "less than the balance available: $ ";
+			cin >> monthlyWithdrawl;
+		}
+		totalWithdrawls = totalWithdrawls + monthlyWithdrawl;
+		currentBal = currentBal - monthlyWithdrawl;
+
+		// Calculate average balance, monthly interest, add to total interest & balance, reset monthlystart
+		double averageBal, monthlyInterest;
+		averageBal = (monthlyStartingBal + currentBal) / 2;
+		monthlyInterest = monthlyIntRate * averageBal;
+		totalIntEarned = totalIntEarned + monthlyInterest;
+		currentBal = currentBal + monthlyInterest;
+		monthlystart = currentBal;
+	}
+	// Create table to display results
+	cout << endl;
+	cout << left << set(35) << "Starting Balance: $"
+		 << right << startingBal << endl;
+	cout << left << set(35) << "Total Deposits: $"
+		 << right << totalDeposits << endl;
+	cout << left << set(35) << "Total Withdrawls: $"
+		 << right << totalWithdrawls << endl;
+	cout << left << set(35) << "Total Interest Earned: $"
+		 << right << totalIntEarned << endl;
+	cout << left << set(35) << "Final Balance: $"
+		 << right << currentBal << endl;
+
+	return 0;
+}
